1f36f31316e90baf82e85b8f45844b0f50394af6
Surender Lohia <surenloh@thoughtworks.com>
2018-06-18 12:16:06 +0530

    Completes redux-saga functionality

---
+++ src/saga.js
@@ -0,0 +1,28 @@
+/*eslint require-yield: "error"*/
+/*eslint-env es6*/
+
+import {takeLatest, put, call} from "redux-saga/effects";
+import axios from "axios";
+
+// watcher saga: watches for actions dispatched to the store, starts worker saga
+export function* watcherSaga() {
+  yield takeLatest('API_CALL_REQUEST', workerSaga);
+}
+
+function fetchDog() {
+  return axios({
+    method: 'get',
+    url: 'https://dog.ceo/api/breeds/image/random'
+  })
+};
+
+function* workerSaga() {
+  try {
+    const response = yield call(fetchDog);
+    const dog = response.data.message;
+    
+    yield put({type: 'API_CALL_SUCCESS', dog});
+  } catch (error) {
+    yield put({type: 'API_CALL_FAILURE', error});
+  }
+}
